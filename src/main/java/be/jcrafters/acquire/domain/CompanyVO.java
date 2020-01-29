1bcaacaa8b45a164a21e4a22ab0bdd9d602f7e1f
Daan Thoné <thonedaan@gmail.com>
2020-01-29 16:38:30 +0100

    CompanyVO

--- src/main/java/be/jcrafters/acquire/domain/CompanyVO.java
+++ src/main/java/be/jcrafters/acquire/domain/CompanyVO.java
@@ -1,4 +1,32 @@
 package be.jcrafters.acquire.domain;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class CompanyVO {
+    public static final int MAX_SHARES = 25;
+    private final CompanyName companyName;
+    private final int remainingShares;
+    private final int size;
+
+    public CompanyVO(CompanyName companyName, int remainingShares, int size) {
+        checkNotNull(companyName);
+        checkArgument(isCorrectRemainingShares(remainingShares));
+        checkArgument(isCorrectSize(size));
+        this.companyName = companyName;
+        this.remainingShares = remainingShares;
+        this.size = size;
+    }
+
+    private boolean isCorrectRemainingShares(int remainingShares) {
+        return remainingShares >= 0 && remainingShares <= MAX_SHARES;
+    }
+
+    private boolean isCorrectSize(int size) {
+        return size > 1;
+    }
+
+    public int getPrice() {
+        return companyName.getPrice(size);
+    }
 }
--- src/test/java/be/jcrafters/acquire/domain/CompanyVOTest.java
+++ src/test/java/be/jcrafters/acquire/domain/CompanyVOTest.java
@@ -1,5 +1,27 @@
 package be.jcrafters.acquire.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class CompanyVOTest {
 
+    @Test
+    void constructor() {
+        assertThatNullPointerException().isThrownBy(() -> new CompanyVO(null, 20, 2));
+
+        assertThatIllegalArgumentException().isThrownBy(() -> new CompanyVO(CompanyName.AMERICA, -1, 2));
+        assertThatIllegalArgumentException().isThrownBy(() -> new CompanyVO(CompanyName.AMERICA, 26, 2));
+
+        assertThatIllegalArgumentException().isThrownBy(() -> new CompanyVO(CompanyName.AMERICA, 20, 1));
+
+        assertThatCode(() -> new CompanyVO(CompanyName.AMERICA, 24, 4)).doesNotThrowAnyException();
+        assertThatCode(() -> new CompanyVO(CompanyName.AMERICA, 24, 10)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void getPrice() {
+        assertThat(new CompanyVO(CompanyName.FUSION, 24, 4).getPrice()).isEqualTo(CompanyName.FUSION.getPrice(4));
+        assertThat(new CompanyVO(CompanyName.PHOENIX, 24, 10).getPrice()).isEqualTo(CompanyName.PHOENIX.getPrice(10));
+    }
 }
